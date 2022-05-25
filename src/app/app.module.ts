b1f4393174f19630e8014702e8d4e95c1e752080
unknown <a38014@adcmuh.org.tw>
2022-05-25 14:26:53 +0800

    加入靜態檔案

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page2Component } from './page2/page2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page2Component
   ],
   imports: [
     BrowserModule,
